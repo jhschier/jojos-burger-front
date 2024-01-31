1a6856b6d19de3469c002534804f1753a7275977
jhschier <jhschier30@hotmail.com>
2024-01-31 21:15:00 +0000

    Feat: Adding Styled-component and globalStyles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import Login from './containers/Login'
+import GlobalStyles from './styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Login />)
+root.render(
+  <>
+    <Login />
+    <GlobalStyles />
+  </>
+)
