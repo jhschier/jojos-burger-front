5e113cf9f3e6a6884a315b371c749cd873e7f13d
jhschier <jhschier30@hotmail.com>
2024-02-28 18:35:28 +0000

    Fix: Updating localStorage store method to function

--- src/hooks/CartContext.js
+++ src/hooks/CartContext.js
@@ -6,6 +6,10 @@ const CartContext = createContext({})
 export const CartProvider = ({ children }) => {
   const [cartProducts, setCartProducts] = useState([])
 
+  const updateLocalStorage = async product => {
+    await localStorage.setItem('jojosburger:cartInfo', JSON.stringify(product))
+  }
+
   const putProductInCart = async product => {
     const cartIndex = cartProducts.findIndex(prd => prd.id === product.id)
 
@@ -21,17 +25,14 @@ export const CartProvider = ({ children }) => {
       newCartProducts = [...cartProducts, product]
       setCartProducts(newCartProducts)
     }
-    await localStorage.setItem(
-      'jojosburger:cartInfo',
-      JSON.stringify(newCartProducts)
-    )
+    await updateLocalStorage(newCartProducts)
   }
   const deleteProduct = async productId => {
     const newCart = cartProducts.filter(product => product.id !== productId)
 
     setCartProducts(newCart)
 
-    await localStorage.setItem('jojosburger:cartInfo', JSON.stringify(newCart))
+    await updateLocalStorage(newCart)
   }
 
   const increaseQuantity = async productId => {
@@ -43,7 +44,7 @@ export const CartProvider = ({ children }) => {
 
     setCartProducts(newCart)
 
-    await localStorage.setItem('jojosburger:cartInfo', JSON.stringify(newCart))
+    await updateLocalStorage(newCart)
   }
 
   const decreaseQuantity = async productId => {
@@ -59,10 +60,7 @@ export const CartProvider = ({ children }) => {
       })
       setCartProducts(newCart)
 
-      await localStorage.setItem(
-        'jojosburger:cartInfo',
-        JSON.stringify(newCart)
-      )
+      await updateLocalStorage(newCart)
     }
   }
 
