58c070c2d8815c898979c49ff43b8f129f49f6a2
jhschier <jhschier30@hotmail.com>
2024-03-07 20:48:40 +0000

    Feat: Creating form for product insertion in New product for Admin.

--- src/containers/Admin/NewProducts/index.js
+++ src/containers/Admin/NewProducts/index.js
@@ -1,18 +1,34 @@
 import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import ReactSelect from 'react-select'
 
 import api from '../../../services/api'
-import { Container } from './styles'
+import { Container, Label, Input, Button } from './styles'
 
 export function NewProduct() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
   useEffect(() => {
     async function createProduct() {
       const { data } = await api.post('products')
     }
-    createProduct()
   }, [])
   return (
     <Container>
-      <div>Oi!</div>
+      <form noValidate>
+        <Label>Name</Label>
+        <Input type="text" {...register('name')} />
+
+        <Label>Price</Label>
+        <Input type="number" {...register('price')} />
+
+        <Label>Image Upload</Label>
+        <Input type="file" accept="image/png, image/jpeg, image/jpg" />
+
+        <ReactSelect />
+
+        <Button>Add Product</Button>
+      </form>
     </Container>
   )
 }
--- src/containers/Admin/NewProducts/styles.js
+++ src/containers/Admin/NewProducts/styles.js
@@ -1,3 +1,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.div``
+
+export const Label = styled.p``
+
+export const Input = styled.input``
+
+export const Button = styled.button``
